b8d1c346b03092681a6444a9eb3d3b2cfd8a21c2
ryotaro_koike <ryotaro_koike@outlook.com>
2019-12-15 03:34:22 +0900

    スタートとゴール地点を選択できるようにした

--- consideration/all_rounder.py
+++ consideration/all_rounder.py
@@ -18,17 +18,64 @@ try:
     from scipy.optimize import curve_fit
 except: pass
 
-class Mouse:
-    def __init__(self, window_name):
-        self.input_img_name = window_name
-        self.mouseEvent = {"x":None, "y":None, "event":None, "flags":None}
+#setup global
+image_frag = True
 
-    def __CallBackFunc(self, eventType, x, y, flags, userdata):
+class Mouse:
+    def __init__(self, window_name, cp_image, org_image):
+        self.cp_image     = cp_image
+        self.org_image    = org_image
+        self.event_call   = 0
+        self.ClickedPoint = [None, None, None, None]
+        
+        self.mouseEvent   = {"x":None, "y":None, "event":None, "flags":None}
+        #cv2.setMouseCallback(window_name, self.__NORMALCallBack, None)
+        cv2.setMouseCallback(window_name, self.__CallBack, None)
+
+    def __NORMALCallBack(self, eventType, x, y, flags, userdata):
+        self.mouseEvent["x"] = x
+        self.mouseEvent["y"] = y
+        self.mouseEvent["event"] = eventType
+        self.mouseEvent["flags"] = flags
+    
+    def __CallBack(self, eventType, x, y, flags, userdata):
         self.mouseEvent["x"] = x
         self.mouseEvent["y"] = y
         self.mouseEvent["event"] = eventType
         self.mouseEvent["flags"] = flags
 
+        if self.mouseEvent["event"] == cv2.EVENT_LBUTTONDOWN:
+            self.event_call += 1
+            M_x = self.mouseEvent["x"]
+            M_y = self.mouseEvent["y"]
+            print(M_x, M_y)
+            global image_frag
+            
+            if self.event_call == 1:
+                image_frag = True
+                self.ClickedPoint[0] = M_x
+                self.ClickedPoint[1] = M_y
+                
+                cv2.circle(self.cp_image, (self.ClickedPoint[0], self.ClickedPoint[1]), 5, (0, 0, 255), -1)
+                cv2.putText(self.cp_image, "START", (self.ClickedPoint[0], self.ClickedPoint[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1)
+            
+            elif self.event_call == 2:
+                self.ClickedPoint[2] = M_x
+                self.ClickedPoint[3] = M_y
+                
+                cv2.circle(self.cp_image, (self.ClickedPoint[2], self.ClickedPoint[3]), 5, (0, 255, 0), -1)
+                cv2.putText(self.cp_image, "GOAL", (self.ClickedPoint[2], self.ClickedPoint[3]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+                
+                #START --> GOAL までの線を引っ張る
+                cv2.line(self.cp_image, (self.ClickedPoint[0], self.ClickedPoint[1]), (self.ClickedPoint[2], self.ClickedPoint[3]),(255, 0, 0), 2)
+                
+            elif self.event_call >= 2:
+                image_frag = False
+                self.event_call = 0
+                self.cp_image = self.org_image
+                print(self.ClickedPoint)
+                self.ClickedPoint = [None, None, None, None]
+                
     def getData(self):
         return self.mouseEvent
 
@@ -48,26 +95,30 @@ class HSV_supporter:
     def __init__(self):
         self.t_init     = False
         self.MB         = True
+        self.fill_holes = False #is not working
         self.opening    = True
         self.closing    = True
         self.ColorErase = False
 
         self.kernel = np.ones((8,8),np.uint8)
+        
+        self.window_name0 = "Serection"
+        self.window_name1 = "Frame"
 
     #トラックバーの初期設定
-    def callback(self, x):
+    def TBcallback(self, x):
         pass
 
     def Trackbars_init(self):
         cv2.namedWindow('value')
-        cv2.createTrackbar('H_Hue','value',179,179,self.callback)
-        cv2.createTrackbar('L_Hue','value',0,179,self.callback)
+        cv2.createTrackbar('H_Hue','value',179,179,self.TBcallback)
+        cv2.createTrackbar('L_Hue','value',0,179,self.TBcallback)
 
-        cv2.createTrackbar('H_Saturation','value',255,255,self.callback)
-        cv2.createTrackbar('L_Saturation','value',0,255,self.callback)
+        cv2.createTrackbar('H_Saturation','value',255,255,self.TBcallback)
+        cv2.createTrackbar('L_Saturation','value',0,255,self.TBcallback)
 
-        cv2.createTrackbar('H_Value','value',255,255,self.callback)
-        cv2.createTrackbar('L_Value','value',0,255,self.callback)
+        cv2.createTrackbar('H_Value','value',255,255,self.TBcallback)
+        cv2.createTrackbar('L_Value','value',0,255,self.TBcallback)
 
     def color_detect(self, hsv, img):
         hsv_min = np.array([15,127,0])
@@ -170,9 +221,6 @@ class HSV_supporter:
         re_image = cv2.resize(img, dsize, fx=X, fy=Y)
         return re_image
 
-    def draw_ClickedPoint(self, M_x, M_y, image):
-        pass
-
     #メイン
     def main(self, videofile_path):
         n = 0
@@ -188,7 +236,30 @@ class HSV_supporter:
             print("INFO : LOAD ERROR ***Chack video path or name.***")
             print("CAP : ", cap)
             exit()
+        
+        #最初に1フレームだけ表示して、STARTとGOALを選択する
+        print("")
+        print("[INFO] : This is the 1st frame.\n[INFO] : Choose start and goal positions and Click.")
+        
+        ret, frame0 = cap.read()
+        cp_frame0 = frame0.copy()
+        
+        cv2.namedWindow(self.window_name0)
+        mouse = Mouse(self.window_name0, cp_frame0, frame0)
+        
+        while True:
+            if image_frag is not True:
+                cp_frame0 = frame0
+                print("change image")
+                
+            cv2.imshow(self.window_name0, cp_frame0)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
+        cv2.destroyAllWindows()
+        
+        #メインのループ
         while(cap.isOpened()):
             ret, frame = cap.read()
             if frame is None:
@@ -221,7 +292,8 @@ class HSV_supporter:
             """
 
             mask = cv2.inRange(hsv, hsv_min, hsv_max)
-
+            
+            #ノイズ除去のためのアルゴリズム判定(True/False)
             if self.MB is True: mask = self.median_blar(mask, 3)
             try:
                 if self.fill_holes is True: mask = self.Fill_Holes(mask)
@@ -245,16 +317,18 @@ class HSV_supporter:
                       thickness=3, lineType=cv2.LINE_AA)
 
             data.append([n, center_x, center_y])
-
-            re_frame=self.resize_image(frame, None, .4, .4)
+            
+            re_frame=self.resize_image(frame, None, .5, .5)
             cv2.imshow("Frame", re_frame)
-            mask = self.resize_image(mask, None, .4, .4)
-            cv2.imshow("image_mask", mask)
+            
+            mask = self.resize_image(mask, None, .5, .5)
+            cv2.imshow("mask image", mask)
+            
             n += 1
-            print(n)
+            
             print("----------")
 
-            if cv2.waitKey(1000) & 0xFF == ord('q'):
+            if cv2.waitKey(10) & 0xFF == ord('q'):
                 break
 
         cap.release()
@@ -263,10 +337,13 @@ class HSV_supporter:
         self.data_plot(data)
 
 if __name__ == '__main__':
-    try:
-        #videofile_path = "20191122/nihongi_f_l1.mp4"
+
+    video = "20191122/nihongi_f_l1.mp4"
+    if len(sys.argv) < 2:
+        videofile_path = video
+    else:
         videofile_path = sys.argv[1]
-    except: print("Not set video")
 
     hsv_sup = HSV_supporter()
     hsv_sup.main(videofile_path)
+    
